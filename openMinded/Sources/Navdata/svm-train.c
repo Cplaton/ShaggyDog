593fe601e3f1cc98d53f6527689980876bdf02e9
JeremyLauvige <jeremy.lauvige@gmail.com>
2014-12-03 09:16:19 +0100

    cleaning svm train - return une structure contenant classe reconnue et confiance

--- openMinded/Sources/Navdata/svm-train.c
+++ openMinded/Sources/Navdata/svm-train.c
@@ -6,41 +6,7 @@
 #include "svm.h"
 #define Malloc(type,n) (type *)malloc((n)*sizeof(type))
 
-//void print_null(const char *s) {}
-
-/*void exit_with_help()
-{
-	printf(
-	"Usage: svm-train [options] training_set_file [model_file]\n"
-	"options:\n"
-	"-s svm_type : set type of SVM (default 0)\n"
-	"	0 -- C-SVC		(multi-class classification)\n"
-	"	1 -- nu-SVC		(multi-class classification)\n"
-	"	2 -- one-class SVM\n"
-	"	3 -- epsilon-SVR	(regression)\n"
-	"	4 -- nu-SVR		(regression)\n"
-	"-t kernel_type : set type of kernel function (default 2)\n"
-	"	0 -- linear: u'*v\n"
-	"	1 -- polynomial: (gamma*u'*v + coef0)^degree\n"
-	"	2 -- radial basis function: exp(-gamma*|u-v|^2)\n"
-	"	3 -- sigmoid: tanh(gamma*u'*v + coef0)\n"
-	"	4 -- precomputed kernel (kernel values in training_set_file)\n"
-	"-d degree : set degree in kernel function (default 3)\n"
-	"-g gamma : set gamma in kernel function (default 1/num_features)\n"
-	"-r coef0 : set coef0 in kernel function (default 0)\n"
-	"-c cost : set the parameter C of C-SVC, epsilon-SVR, and nu-SVR (default 1)\n"
-	"-n nu : set the parameter nu of nu-SVC, one-class SVM, and nu-SVR (default 0.5)\n"
-	"-p epsilon : set the epsilon in loss function of epsilon-SVR (default 0.1)\n"
-	"-m cachesize : set cache memory size in MB (default 100)\n"
-	"-e epsilon : set tolerance of termination criterion (default 0.001)\n"
-	"-h shrinking : whether to use the shrinking heuristics, 0 or 1 (default 1)\n"
-	"-b probability_estimates : whether to train a SVC or SVR model for probability estimates, 0 or 1 (default 0)\n"
-	"-wi weight : set the parameter C of class i to weight*C, for C-SVC (default 1)\n"
-	"-v n: n-fold cross validation mode\n"
-	"-q : quiet mode (no outputs)\n"
-	);
-	exit(1);
-}*/
+void read_problem(const char *filename);
 
 void exit_input_errors(int line_num)
 {
@@ -84,7 +50,7 @@ double do_cross_validation()
 	double total_error = 0;
 	double sumv = 0, sumy = 0, sumvv = 0, sumyy = 0, sumvy = 0;
 	double *target = Malloc(double,prob.l);
-    double accuracy;
+    double accuracy = 0;
     svm_cross_validation(&prob,&param,nr_fold,target);
 	if(param.svm_type == EPSILON_SVR ||
 	   param.svm_type == NU_SVR)
@@ -153,38 +119,26 @@ float * compute_parameters(char* training_set, int folds){
  
     char* input_file_name=training_set;
     const char *error_msg;
-    float gamma,C, gamma_aux,C_aux;
+    float gamma,C,C_aux;
+	float gamma_aux = 0.0;
     double aux, accuracy;
     float * param_aux=malloc(16);
     printf("dans compute, nb_indiv=%d",nb_indiv);
     aux = 0.0;
     C=1;
-    //while(C<10){
         gamma=nb_indiv/6;
         while(gamma<nb_indiv/3){
             create_model(folds,gamma,C);
             read_problem(input_file_name);
             error_msg = svm_check_parameter(&prob,&param);
             accuracy = do_cross_validation();
-//                printf("gamma = %f\n",gamma);
-//                printf("C = %f\n",C);
-            //matlab = fopen("stats.m","w+");
-            //fseek(matlab,2,1);
-//           fprintf(matlab,"%f %lf;\n",gamma,accuracy);
-          //  fclose(matlab);
             if(accuracy > aux){
                 aux=accuracy;
                 gamma_aux=gamma;
                 C_aux=C;
-//                printf("\rAccuracy=%lf",aux);
             }
-//            else{
-//                printf("\rBad Accuracy = %lf",accuracy);
-//            }
             gamma++;
         }
-        //C+=10;
-    //}
     printf("\n");
     printf("final accuracy = %lf%%, C=%f, gamma=%f\n",aux,C_aux,gamma_aux);
     param_aux[0]=gamma_aux;
@@ -192,123 +146,6 @@ float * compute_parameters(char* training_set, int folds){
     return param_aux;
 }
 
-
-/*void parse_command_line(int argc, char **argv, char *input_file_name, char *model_file_name)
-{
-	int i;
-	void (*print_func)(const char*) = NULL;	// default printing to stdout
-
-	// default values
-	param.svm_type = C_SVC;
-	param.kernel_type = RBF;
-	param.degree = 3;
-	param.gamma = 0;	// 1/num_features
-	param.coef0 = 0;
-	param.nu = 0.5;
-	param.cache_size = 100;
-	param.C = 1;
-	param.eps = 1e-3;
-	param.p = 0.1;
-	param.shrinking = 1;
-	param.probability = 0;
-	param.nr_weight = 0;
-	param.weight_label = NULL;
-	param.weight = NULL;
-	cross_validation = 0;
-
-	// parse options
-	for(i=1;i<argc;i++)
-	{
-		if(argv[i][0] != '-') break;
-		if(++i>=argc)
-			exit_with_help();
-		switch(argv[i-1][1])
-		{
-			case 's':
-				param.svm_type = atoi(argv[i]);
-				break;
-			case 't':
-				param.kernel_type = atoi(argv[i]);
-				break;
-			case 'd':
-				param.degree = atoi(argv[i]);
-				break;
-			case 'g':
-				param.gamma = atof(argv[i]);
-				break;
-			case 'r':
-				param.coef0 = atof(argv[i]);
-				break;
-			case 'n':
-				param.nu = atof(argv[i]);
-				break;
-			case 'm':
-				param.cache_size = atof(argv[i]);
-				break;
-			case 'c':
-				param.C = atof(argv[i]);
-				break;
-			case 'e':
-				param.eps = atof(argv[i]);
-				break;
-			case 'p':
-				param.p = atof(argv[i]);
-				break;
-			case 'h':
-				param.shrinking = atoi(argv[i]);
-				break;
-			case 'b':
-				param.probability = atoi(argv[i]);
-				break;
-			case 'q':
-				print_func = &print_null;
-				i--;
-				break;
-			case 'v':
-				cross_validation = 1;
-				nr_fold = atoi(argv[i]);
-				if(nr_fold < 2)
-				{
-					fprintf(stderr,"n-fold cross validation: n must >= 2\n");
-					exit_with_help();
-				}
-				break;
-			case 'w':
-				++param.nr_weight;
-				param.weight_label = (int *)realloc(param.weight_label,sizeof(int)*param.nr_weight);
-				param.weight = (double *)realloc(param.weight,sizeof(double)*param.nr_weight);
-				param.weight_label[param.nr_weight-1] = atoi(&argv[i-1][2]);
-				param.weight[param.nr_weight-1] = atof(argv[i]);
-				break;
-			default:
-				fprintf(stderr,"Unknown option: -%c\n", argv[i-1][1]);
-				exit_with_help();
-		}
-	}
-
-	svm_set_print_string_function(print_func);
-
-	// determine filenames
-
-	if(i>=argc)
-		exit_with_help();
-
-	strcpy(input_file_name, argv[i]);
-
-	if(i<argc-1)
-		strcpy(model_file_name,argv[i+1]);
-	else
-	{
-		char *p = strrchr(argv[i],'/');
-		if(p==NULL)
-			p = argv[i];
-		else
-			++p;
-		sprintf(model_file_name,"%s.model",p);
-	}
-}*/
-
-// read in a problem (in svmlight format)
 
 void read_problem(const char *filename)
 {
@@ -414,7 +251,7 @@ void read_problem(const char *filename)
 	fclose(fp);
 }
 
-int training_model_generation(char* training_set, char* training_model, int folds, int nb_specimen)
+void training_model_generation(char* training_set, char* training_model, int folds, int nb_specimen)
 {
 	char* input_file_name=training_set;
 	char* model_file_name=training_model;
@@ -426,14 +263,7 @@ int training_model_generation(char* training_set, char* training_model, int fold
     create_model(0,1,1);
 	read_problem(input_file_name);
 	error_msg = svm_check_parameter(&prob,&param);
- //   matlab = fopen("stats.m","w+");
- //   fprintf(matlab,"accuracy = [");
- //   fclose(matlab);
     parameters = compute_parameters(training_set, folds);
- //   matlab = fopen("stats.m","w+");
- //   fseek(matlab,2,1);
- //   fprintf(matlab,"];");
- //   fclose(matlab);
     printf("gamma=%f C=%f\n",parameters[0],parameters[1]); 
     create_model(0,parameters[0],parameters[1]);
     printf("model créé\n");
@@ -464,47 +294,4 @@ int training_model_generation(char* training_set, char* training_model, int fold
 	free(prob.x);
 	free(x_space);
 	free(line);
-
-	return 0;
 }
-
-/*int main(int argc, char **argv)
-{
-	char input_file_name[1024];
-	char model_file_name[1024];
-	const char *error_msg;
-
-	parse_command_line(argc, argv, input_file_name, model_file_name);
-	read_problem(input_file_name);
-	error_msg = svm_check_parameter(&prob,&param);
-
-	if(error_msg)
-	{
-		fprintf(stderr,"ERROR: %s\n",error_msg);
-		exit(1);
-	}
-
-	if(cross_validation)
-	{
-		do_cross_validation();
-	}
-	else
-	{
-		model = svm_train(&prob,&param);
-		if(svm_save_model(model_file_name,model))
-		{
-			fprintf(stderr, "can't save model to file %s\n", model_file_name);
-			exit(1);
-		}
-		svm_free_and_destroy_model(&model);
-	}
-	svm_destroy_param(&param);
-	free(prob.y);
-	free(prob.x);
-	free(x_space);
-	free(line);
-
-	return 0;
-}*/
-
-
