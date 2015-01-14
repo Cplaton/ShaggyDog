66d31ae8768524935d3a511acc5055dd08df01a3
Cplaton <clement.platon@gmail.com>
2015-01-14 11:36:36 +0100

    Changenement de nom du main

---
+++ openMinded/Sources/openminded_main.h
@@ -0,0 +1,19 @@
+/**
+ * @file    openminded_main.h
+ * @author  ShaggyDogs and Smartfox
+ * @brief   OpendMINDED's main file based on Smartfox's file
+ * @version 2.0
+ * @date    November 2014
+ **/
+#ifndef _MYKONOS_TESTING_TOOL_H_
+#define _MYKONOS_TESTING_TOOL_H_
+
+#include <stdio.h>
+#include <VP_Os/vp_os_types.h>
+
+C_RESULT signal_exit();
+
+/* request to stop the app */
+void stop_request();
+
+#endif // _MYKONOS_TESTING_TOOL_H_
