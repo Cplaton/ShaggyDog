de8f48156fab0fb398d5c44e74ef2f3cc0288c23
Cplaton <clement.platon@gmail.com>
2015-01-16 17:31:23 +0100

    un peu plus de doxygen

---
+++ openMinded/dox.h
@@ -0,0 +1,34 @@
+/** @mainpage Index Page
+ *
+ *  @section intro_sec Abstract
+ * 
+ *  <img src="./openmind.png" width=20%/>
+ * OPENMINDED is the name of a security add-on for the PARROT’s AR Drone 2.0. It is was created by a team 
+ * of software engineers in the department of electronics and informatics at the INSA Toulouse. It integrates a real time detection 
+ * service, machine learning algorithms and statistical classification, all implemented in order to get a much 
+ * secure drone. 
+ * 
+ * Our product integrates three different classification algorithms: KNN (K Nearest Neighbors), SVM (Support 
+ * Vector Machine) and NBC (Naïve Bayes Classifier). We provided an easy to use graphical interface. The 
+ * user may view the current situation of the drone, therefore knowing any obstacle the drone may 
+ * encounter. In case of an accident, an integrated reaction module will perform the required procedure in 
+ * order to avoid the obstacle ergo preventing any danger. 
+ * 
+ * For the experienced users, our product can be modified in order to add more classified situations for a 
+ * very secure drone. 
+ * 
+ * Our project offers an application for both the casual user and the experienced programmer in order to 
+ * avoid any dangerous situation that the drone may encounter during the flight mode.
+ *
+ * <video width=40% controls>
+ * <source src="DroneFinal.mp4" type="video/mp4">
+ * </video>
+ *
+ *  @section install_sec How to use OpenMINDED
+ * 
+ *  Check out our repository at https://github.com/Cplaton/ShaggyDog/
+ *
+ *  For more information, you can have a look on our cookbook and documentation files which are in the "doc" folder of the openMINDED project. 
+ *  
+ *  
+ **/
